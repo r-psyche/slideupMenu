7e89c779cbf6149d1a5894ed7ea6aeb65693080c
r-psyche <u.rose0409@gmail.com>
2020-09-17 12:18:11 +0900

    add animation

--- slideupMenu/slideupMenu/SlideUpMenu.swift
+++ slideupMenu/slideupMenu/SlideUpMenu.swift
@@ -12,6 +12,10 @@ import UIKit
 class SlideUpMenu: UIView {
     
     @IBOutlet var slideupView: UIView!
+    @IBOutlet var stackView: UIStackView!
+    @IBOutlet var firstView: UIView!
+    @IBOutlet var secondView: UIView!
+    @IBOutlet var thirdView: UIView!
     
     private var isShow:Bool = false
     private let slideUpViewHeight: CGFloat = 250
@@ -32,21 +36,42 @@ class SlideUpMenu: UIView {
         parentView.addSubview(slideupView)
         
         slideupView.widthAnchor.constraint(equalToConstant: screenSize.width).isActive = true
-        slideupView.heightAnchor.constraint(equalToConstant: self.slideUpViewHeight - 70).isActive = true
+        slideupView.heightAnchor.constraint(equalToConstant: self.slideUpViewHeight).isActive = true
         slideupView.centerYAnchor.constraint(equalTo: parentView.layoutMarginsGuide.bottomAnchor).isActive = true
+        
+        setupHideMenu()
+    }
+    
+    func setupHideMenu() {
+        self.firstView.isHidden = true
+        self.secondView.isHidden = false
+        self.thirdView.isHidden = true
+    }
+    
+    func setupShowMenu() {
+        self.firstView?.isHidden = false
+        self.secondView?.isHidden = true
+        self.thirdView?.isHidden = false
     }
     
     func touchSlideUpMenu() {
         if isShow == false {
-            UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut], animations: {
-                self.slideupView.frame = CGRect(x: 0, y: self.screenSize.height - self.slideUpViewHeight - 100, width: self.screenSize.width, height: self.slideUpViewHeight)
-            }, completion: nil)
-            self.isShow = true
+            UIView.animate(withDuration: 0, animations: {
+                self.setupShowMenu()
+            }) { (success) in
+                UIView.animate(withDuration: 0.1, delay: 0, options: [.curveEaseInOut], animations: {
+                    self.slideupView.frame = CGRect(x: 0, y: self.screenSize.height - self.slideUpViewHeight - 100, width: self.screenSize.width, height: self.slideUpViewHeight)
+                }, completion: { success in
+                    self.isShow = true
+                })
+            }
         } else {
-            UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut], animations: {
-                self.slideupView.frame = CGRect(x: 0, y: self.screenSize.height - self.slideUpViewHeight + 30, width: self.screenSize.width, height: self.slideUpViewHeight)
-            }, completion: nil)
-            self.isShow = false
+            UIView.animate(withDuration: 0.1, delay: 0, options: [.curveEaseInOut, .beginFromCurrentState], animations: {
+                self.slideupView.frame = CGRect(x: 0, y: self.screenSize.height - self.slideUpViewHeight + 10, width: self.screenSize.width, height: self.slideUpViewHeight)
+            }, completion: { success in
+                self.setupHideMenu()
+                self.isShow = false
+            })
         }
         
     }
